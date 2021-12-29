3e3d8ced8a18321c2efb70aa5541f59e8cd934fc
cedricdc <>
2021-12-29 13:18:31 +0100

    [day 14] implement sequential method

--- day_14/puzzle_01/main.cpp
+++ day_14/puzzle_01/main.cpp
@@ -5,6 +5,8 @@
 #include <list>
 #include <map>
 #include <string>
+#include <utility>
+#include <vector>
 
 // Assumpions:
 // - every possible pair of characters has an insertion
@@ -36,6 +38,92 @@ void printMapping(const PolyMap& mapping) {
   for (const auto& [key, value] : mapping) {
     std::cout << key << " --> " << value << std::endl;
   }
+}
+
+std::map<char, std::size_t> buildPolymerBruteForce(const PolyMap& mapping,
+                                                   const std::string& start_string,
+                                                   std::size_t num_steps) {
+  std::list<char> final_string;
+  std::map<char, std::size_t> char_counter;
+
+  for (auto c : start_string) {
+    final_string.push_back(c);
+    ++char_counter[c];
+  }
+
+  for (std::size_t iter = 0; iter < num_steps; ++iter) {
+    std::string key(2, ' ');
+
+    auto c_iter = final_string.begin();
+    key[1] = *c_iter;
+    while (++c_iter != final_string.end()) {
+      key[0] = key[1];
+      key[1] = *c_iter;
+
+      char inserted = mapping.at(key);
+      final_string.insert(c_iter, inserted);
+      ++char_counter[inserted];
+    }
+  }
+
+  // print polymer
+  //  std::cout << "Final polymer: " << std::endl;
+  //  for (auto c : final_string) std::cout << c;
+  //  std::cout << std::endl;
+
+  return char_counter;
+}
+
+std::map<char, std::size_t> buildPolymerSegmented(const PolyMap& mapping,
+                                                  const std::string& start_string,
+                                                  std::size_t num_steps) {
+  using Operation = std::pair<char, int>;  // end character, and how many steps to take
+  std::map<char, std::size_t> char_counter;
+  std::vector<Operation> operation_stack;
+
+  std::string final_string;
+  auto print_stack = [](const std::vector<Operation>& stack) {
+    for (auto elem : stack) std::cout << "(" << elem.first << ", " << elem.second << "), ";
+    std::cout << std::endl;
+  };
+  auto print_final = [&]() {
+    for (auto crit = final_string.crbegin(); crit != final_string.crend(); ++crit)
+      std::cout << *crit << " ";
+    std::cout << std::endl;
+  };
+
+  std::cout << "Starting stack: " << std::endl;
+  print_stack(operation_stack);
+
+  for (auto c : start_string) operation_stack.emplace_back(c, num_steps);
+
+  std::string key(2, ' ');
+  while (operation_stack.size() > 1) {
+    Operation op = operation_stack.back();
+    operation_stack.pop_back();
+    key[0] = operation_stack.back().first;
+    key[1] = op.first;
+
+    for (int i = op.second - 1; i >= 0; --i) {
+      // newly generated character is start of next pair
+      char inserted = mapping.at(key);
+
+      if (i > 0) {
+        key[0] = inserted;
+        operation_stack.emplace_back(inserted, i);
+      } else {
+        ++char_counter[key[1]];
+        ++char_counter[inserted];
+        final_string.push_back(key[1]);
+        final_string.push_back(inserted);
+      }
+    }
+  }
+
+  final_string.push_back(operation_stack.front().first);
+  print_final();
+
+  return char_counter;
 }
 
 }  // namespace
@@ -75,27 +163,12 @@ int main(int argc, char** argv) {
   /*
    * option 1 : brute force
    */
-  std::list<char> final_string;
-  std::map<char, std::size_t> char_counter;
-  for (auto c : start_string) {
-    final_string.push_back(c);
-    ++char_counter[c];
-  }
+  //  auto char_counter = buildPolymerBruteForce(mapping, start_string, num_steps);
 
-  for (std::size_t iter = 0; iter < num_steps; ++iter) {
-    std::string key(2, ' ');
-
-    auto c_iter = final_string.begin();
-    key[1] = *c_iter;
-    while (++c_iter != final_string.end()) {
-      key[0] = key[1];
-      key[1] = *c_iter;
-
-      char inserted = mapping.at(key);
-      final_string.insert(c_iter, inserted);
-      ++char_counter[inserted];
-    }
-  }
+  /*
+   * option 2 : Approach for big polymers, cannot save whole thing in memory
+   */
+  auto char_counter = buildPolymerSegmented(mapping, start_string, num_steps);
 
   // compute desired result
   std::size_t most_common = 0;
